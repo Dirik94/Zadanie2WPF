07b804ab0ad55405b6ae10616f73737c509584fa
Grzegorz Domanowski <grzegorz.domanowski.ais@gmail.com>
2022-03-23 19:05:21 +0100

    Dodaj pliki projektów.

---
+++ Zadanie2WPF/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie2WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Order order;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            order = new Order();
+        }
+
+        private void DisplaySummary()
+        {
+            TextBox_Summary.Text = order.ToString();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result;
+            result = MessageBox.Show("Dziękujemy za złożenie zamówienia", "Zamówienie Przyjęto", MessageBoxButton.OK);
+
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ColorPaper_Check(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = ((CheckBox)sender).IsChecked ?? false;
+            if (isChecked)
+            {
+                ComboBox_Colors.IsEnabled = isChecked;
+                order.Bonus += 0.5;
+            }
+            DisplaySummary();
+        }
+
+        private void ColorPaper_Uncheck(object sender, RoutedEventArgs e)
+        {
+            bool isChecked = ((CheckBox)sender).IsChecked ?? false;
+            if (isChecked == false)
+            {
+                ComboBox_Colors.IsEnabled = isChecked;
+                order.Bonus -= 0.5;
+            }
+            DisplaySummary();
+        }
+
+        private void BonusCheck(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            if (cb.IsChecked == true)
+            {
+                order.Bonus += double.Parse(cb.Tag.ToString());
+            }
+            else
+            {
+                order.Bonus -= double.Parse(cb.Tag.ToString());
+            }
+            DisplaySummary();
+        }
+        private void ZmianaGramatury(object sender, RoutedEventArgs e)
+        {
+
+            if (((RadioButton)sender).Tag == "80")
+            {
+                order.NumerGramatury = 1;
+            }
+            else if (((RadioButton)sender).Tag == "120")
+            {
+                order.NumerGramatury = 2;
+            }
+            else if (((RadioButton)sender).Tag == "200")
+            {
+                order.NumerGramatury = 2.5;
+            }
+            else
+            {
+                order.NumerGramatury = 3;
+            }
+                order.Gramatura = ((RadioButton)sender).Tag + " g/m²";
+            DisplaySummary();
+        }
+
+        private void ZmianaCzasWykonania(object sender, RoutedEventArgs e)
+        {
+            if(((RadioButton)sender).Tag.ToString() == "true"){
+                order.CzasWykonaniaAddedCost = 15;
+                order.CzasWykonania = "Ekspresowy";
+            }
+            else
+            {
+                order.CzasWykonaniaAddedCost = 0;
+                order.CzasWykonania = "Standardowy";
+            }
+            DisplaySummary();
+        }
+
+        private void ZmianaKoloruDruku(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            if (cb.IsEnabled == false && order.Color != "Biały")
+            {
+                order.Color = "Biały";
+            }
+            else
+            {
+                cb.IsEnabled = true;
+                order.Color = cb.SelectedItem.ToString();
+            }
+            DisplaySummary();
+        }
+
+        private void ZmianaNaSlider(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            order.BasePrice = 0.2;
+
+            if (Math.Round(((Slider)sender).Value) == 0)
+            {
+                Label_Format.Content = "A" + "0" + " - cena " + order.BasePrice * 100 + "gr/szt.";
+            }
+            else if (Math.Round(((Slider)sender).Value) == 1)
+            {
+                order.BasePrice *= 2.5;
+                Label_Format.Content = "A" + "1" + " - cena " + order.BasePrice * 100 + "gr/szt.";
+            }
+            else if(Math.Round(((Slider)sender).Value) == 2)
+            {
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                Label_Format.Content = "A" + "2" + " - cena " + order.BasePrice + "zł/szt.";
+            }
+            else if (Math.Round(((Slider)sender).Value) == 3)
+            {
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                Label_Format.Content = "A" + "3" + " - cena " + Math.Round(order.BasePrice,2) + "zł/szt.";
+            }
+            else if (Math.Round(((Slider)sender).Value) == 4)
+            {
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                Label_Format.Content = "A" + "4" + " - cena " + Math.Round(order.BasePrice,2) + "zł/szt.";
+            }
+            else if (Math.Round(((Slider)sender).Value) == 5)
+            {
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                order.BasePrice *= 2.5;
+                Label_Format.Content = "A" + "5" + " - cena " + Math.Round(order.BasePrice,2) + "zł/szt.";
+            }
+            DisplaySummary();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                TextBox_Summary.Text = "";
+            }
+            else
+            {
+                order.Count = int.Parse(textBox.Text);
+                DisplaySummary();
+            }
+        }
+
+        private void ValidateText(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
---
+++ Zadanie2WPF/Order.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Zadanie2WPF
+{
+    internal class Order
+    {
+        public int Count { get; set; } = 0;
+        public string Format { get; set; } = "A5";
+        public string Gramatura { get; set; } = "80 g/m²";
+        public string PrintType { get; set; } = "Normalny";
+        public string Color { get; set; } = "Biały";
+        public string CzasWykonania { get; set; } = "Standard";
+        public double Discount
+        {
+            get
+            {
+                if(Count >= 100 && Count < 200)
+                {
+                    return 1;
+                }
+                else if (Count >= 200 && Count < 300)
+                {
+                    return 2;
+                }
+                else if(Count >= 300 && Count < 400)
+                {
+                    return 3;
+                }
+                else if (Count >= 400 && Count < 500)
+                {
+                    return 4;
+                }
+                else if (Count >= 500 && Count < 600)
+                {
+                    return 5;
+                }
+                else if (Count >= 600 && Count < 700)
+                {
+                    return 6;
+                }
+                else if (Count >= 700 && Count < 800)
+                {
+                    return 7;
+                }
+                else if (Count >= 800 && Count < 900)
+                {
+                    return 8;
+                }
+                else if (Count >= 900 && Count < 1000)
+                {
+                    return 9;
+                }
+                else if (Count >= 1000)
+                {
+                    return 10;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            private set { }
+        }
+        public double FinalPrice
+        {
+            get
+            {
+                return Count * BasePrice * Bonus * (1 - (Discount/10)/10) * NumerGramatury + CzasWykonaniaAddedCost;
+            }
+            private set { }
+        }
+
+        public double BasePriceNoDiscount
+        {
+            get
+            {
+                return Count * BasePrice * Bonus * NumerGramatury + CzasWykonaniaAddedCost;
+            }
+            private set { }
+        }
+
+        public double BasePrice { get; set; } = 0.2;
+        public double NumerGramatury { get; set; } = 1;
+        public double Bonus { get; set; } = 1;
+        public int CzasWykonaniaAddedCost { get; set; } = 0;
+
+        public override string ToString()
+        {
+            return "Przedmiot zamówienia: " + Count + " szt." + ", Format: " + Format + ", Gramatura: " + Gramatura + "\n" + "Czas realizacji: " + CzasWykonania + ", Papier: " + Color + "\n"
+                + "Cena przed rabatem: " + Math.Round(BasePriceNoDiscount,2) + "zł\n" + "Naliczony rabat: " + Discount + "%\n"
+                + "Cena po rabacie: " + Math.Round(FinalPrice, 2).ToString("0.00") + "zł";
+        }
+    }
+}
